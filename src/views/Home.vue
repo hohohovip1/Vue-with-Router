b8a2aca8ef08e2d00b459e4d4539aa665d6d2994
Nova <hohohovip1@gmail.com>
2021-03-11 16:45:52 +0700

    vue without database

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos doloremque,
+      delectus corrupti ullam non rerum corporis quam accusamus eligendi
+      quibusdam illo quas eaque, pariatur iste totam sed alias, magnam ipsum?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis hic
+      accusantium veniam exercitationem error nam ab aut recusandae, aliquid sed
+      velit, veritatis, ipsam eligendi ut fugit assumenda voluptatem quod
+      beatae.
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+};
 </script>
